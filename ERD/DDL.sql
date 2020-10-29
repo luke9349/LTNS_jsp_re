957b03872ac6a2bc96d12bd2785068d57270df2b
홍성혁 <markhong93@naver.com>
2020-10-29 13:50:20 +0900

    DB 생성 쿼리 완성

--- ERD/DDL.sql
+++ ERD/DDL.sql
@@ -1,24 +1,19 @@
 
 /* Drop Triggers */
 
-DROP TRIGGER TRI_book_board_post_id;
 DROP TRIGGER TRI_commenttable_comment_id;
 DROP TRIGGER TRI_comment_comment_id;
 DROP TRIGGER TRI_comment_table_comment_id;
 DROP TRIGGER TRI_filetable_file_id;
 DROP TRIGGER TRI_file_file_id;
 DROP TRIGGER TRI_file_table_file_id;
-DROP TRIGGER TRI_game_board_post_id;
 DROP TRIGGER TRI_membermanagement_mm_id;
 DROP TRIGGER TRI_member_mem_id;
 DROP TRIGGER TRI_mmtable_mm_id;
 DROP TRIGGER TRI_mm_table_mm_id;
-DROP TRIGGER TRI_movie_board_post_id;
-DROP TRIGGER TRI_notice_board_post_id;
 DROP TRIGGER TRI_posttable_post_id;
 DROP TRIGGER TRI_post_post_id;
 DROP TRIGGER TRI_post_table_post_id;
-DROP TRIGGER TRI_sports_board_post_id;
 
 
 
@@ -34,68 +29,57 @@ DROP TABLE mm_table CASCADE CONSTRAINTS;
 
 /* Drop Sequences */
 
-DROP SEQUENCE SEQ_book_board_post_id;
 DROP SEQUENCE SEQ_commenttable_comment_id;
 DROP SEQUENCE SEQ_comment_comment_id;
 DROP SEQUENCE SEQ_comment_table_comment_id;
 DROP SEQUENCE SEQ_filetable_file_id;
 DROP SEQUENCE SEQ_file_file_id;
 DROP SEQUENCE SEQ_file_table_file_id;
-DROP SEQUENCE SEQ_game_board_post_id;
 DROP SEQUENCE SEQ_membermanagement_mm_id;
 DROP SEQUENCE SEQ_member_mem_id;
 DROP SEQUENCE SEQ_mmtable_mm_id;
 DROP SEQUENCE SEQ_mm_table_mm_id;
-DROP SEQUENCE SEQ_movie_board_post_id;
-DROP SEQUENCE SEQ_notice_board_post_id;
 DROP SEQUENCE SEQ_posttable_post_id;
 DROP SEQUENCE SEQ_post_post_id;
 DROP SEQUENCE SEQ_post_table_post_id;
-DROP SEQUENCE SEQ_sports_board_post_id;
 
 
 
 
 /* Create Sequences */
 
-CREATE SEQUENCE SEQ_book_board_post_id INCREMENT BY 1 START WITH 1;
 CREATE SEQUENCE SEQ_commenttable_comment_id INCREMENT BY 1 START WITH 1;
 CREATE SEQUENCE SEQ_comment_comment_id INCREMENT BY 1 START WITH 1;
 CREATE SEQUENCE SEQ_comment_table_comment_id INCREMENT BY 1 START WITH 1;
 CREATE SEQUENCE SEQ_filetable_file_id INCREMENT BY 1 START WITH 1;
 CREATE SEQUENCE SEQ_file_file_id INCREMENT BY 1 START WITH 1;
 CREATE SEQUENCE SEQ_file_table_file_id INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE SEQ_game_board_post_id INCREMENT BY 1 START WITH 1;
 CREATE SEQUENCE SEQ_membermanagement_mm_id INCREMENT BY 1 START WITH 1;
 CREATE SEQUENCE SEQ_member_mem_id INCREMENT BY 1 START WITH 1;
 CREATE SEQUENCE SEQ_mmtable_mm_id INCREMENT BY 1 START WITH 1;
 CREATE SEQUENCE SEQ_mm_table_mm_id INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE SEQ_movie_board_post_id INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE SEQ_notice_board_post_id INCREMENT BY 1 START WITH 1;
 CREATE SEQUENCE SEQ_posttable_post_id INCREMENT BY 1 START WITH 1;
 CREATE SEQUENCE SEQ_post_post_id INCREMENT BY 1 START WITH 1;
 CREATE SEQUENCE SEQ_post_table_post_id INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE SEQ_sports_board_post_id INCREMENT BY 1 START WITH 1;
 
 
 
+
+DROP DOMAIN GRADE_D CASCADE;
 
 /* Create Domains */
 -- 회원 등급
-CREATE DOMAIN _GRADE VARCHAR2(32)
-   CONSTRAINT valid-grade CHECK (VALUE IN ('admin','member'));
-
-
+CREATE DOMAIN GRADE_D VARCHAR2(30)
+   CONSTRAINT valid-grade CHECK(VALUE IN('admin','member'));
+  
 -- 게시판 카테고리
-CREATE DOMAIN _CATEGORY VARCHAR2(32)
-   CONSTRAINT valid-category CHECK (VALUE IN ('notice','movie','book','sports','game'));
+CREATE DOMAIN _CATEGORY VARCHAR2(30)
+   CONSTRAINT valid-category CHECK (VALUE IN('notice','movie','book','sports','game'));
 
 
 -- 파일 분류
-CREATE DOMAIN _FILEKIND VARCHAR2(32)
-   CONSTRAINT valid-filekind CHECK (VALUE IN ('file','image','postcontents'));
-
-
+CREATE DOMAIN _FILEKIND VARCHAR2(30)
+   CONSTRAINT valid-filekind CHECK (VALUE IN('file','image','postcontents'));
 
 
 /* Create Tables */
@@ -136,7 +120,7 @@ CREATE TABLE file_table
    -- 게시글 고유번호
    post_id number NOT NULL,
    -- 분류 : _FILEKIND 도메인을 사용
-   filekind _FILEKIND NOT NULL,
+   filekind varchar2(30) NOT NULL,
    -- 실제파일명
    real_filename varchar2(256) NOT NULL,
    -- 업로드 파일명
@@ -144,12 +128,11 @@ CREATE TABLE file_table
    PRIMARY KEY (file_id)
 );
 
-
 -- 회원
 CREATE TABLE mm_table
 (
    -- 회원고유번호
-   mm_id number NOT NULL UNIQUE,
+   mm_id number NOT NULL,
    -- 회원id : 영문 대소문자 숫자 조합
    id varchar2(20) NOT NULL UNIQUE,
    -- 비밀번호 : 특수문자 영문 숫자 8~25
@@ -159,10 +142,9 @@ CREATE TABLE mm_table
    -- 이메일 : 이메일 형식 @ 검사
    email varchar2(100) NOT NULL UNIQUE,
    -- 등급 : 도메인 grade 사용
-   grade  _GRADE NOT NULL,
+   grade  varchar2(30) NOT NULL,
    PRIMARY KEY (mm_id)
 );
-
 
 -- 게시글
 CREATE TABLE post_table
@@ -174,7 +156,7 @@ CREATE TABLE post_table
    -- 작성자 : member 테이블의 mem_id 외래키
    writer number NOT NULL,
    -- 카테고리 : _CATEGORY  도메인을 따름
-   category _CATEGORY NOT NULL,
+   category varchar2(30) NOT NULL,
    -- 작성일
    regdate timestamp NOT NULL,
    -- 조회수
@@ -182,7 +164,7 @@ CREATE TABLE post_table
    PRIMARY KEY (post_id)
 );
 
-
+COMMIT;
 
 /* Create Foreign Keys */
 
@@ -227,65 +209,24 @@ ALTER TABLE file_table
       ON DELETE CASCADE
 ;
 
+COMMIT;
 
+
+
+/* 트리거 생성전 실행 필수 */
+SELECT * FROM comment_table;
+SELECT * FROM empathize_table;
+SELECT * FROM file_table;
+SELECT * FROM mm_table;
+SELECT * FROM post_table;
 
 /* Create Triggers */
-
-CREATE OR REPLACE TRIGGER TRI_book_board_post_id BEFORE INSERT ON book_board
-FOR EACH ROW
-BEGIN
-   SELECT SEQ_book_board_post_id.nextval
-   INTO :new.post_id
-   FROM dual;
-END;
-
-/
-
-CREATE OR REPLACE TRIGGER TRI_commenttable_comment_id BEFORE INSERT ON commenttable
-FOR EACH ROW
-BEGIN
-   SELECT SEQ_commenttable_comment_id.nextval
-   INTO :new.comment_id
-   FROM dual;
-END;
-
-/
-
-CREATE OR REPLACE TRIGGER TRI_comment_comment_id BEFORE INSERT ON comment
-FOR EACH ROW
-BEGIN
-   SELECT SEQ_comment_comment_id.nextval
-   INTO :new.comment_id
-   FROM dual;
-END;
-
-/
 
 CREATE OR REPLACE TRIGGER TRI_comment_table_comment_id BEFORE INSERT ON comment_table
 FOR EACH ROW
 BEGIN
    SELECT SEQ_comment_table_comment_id.nextval
    INTO :new.comment_id
-   FROM dual;
-END;
-
-/
-
-CREATE OR REPLACE TRIGGER TRI_filetable_file_id BEFORE INSERT ON filetable
-FOR EACH ROW
-BEGIN
-   SELECT SEQ_filetable_file_id.nextval
-   INTO :new.file_id
-   FROM dual;
-END;
-
-/
-
-CREATE OR REPLACE TRIGGER TRI_file_file_id BEFORE INSERT ON file
-FOR EACH ROW
-BEGIN
-   SELECT SEQ_file_file_id.nextval
-   INTO :new.file_id
    FROM dual;
 END;
 
@@ -301,91 +242,11 @@ END;
 
 /
 
-CREATE OR REPLACE TRIGGER TRI_game_board_post_id BEFORE INSERT ON game_board
-FOR EACH ROW
-BEGIN
-   SELECT SEQ_game_board_post_id.nextval
-   INTO :new.post_id
-   FROM dual;
-END;
-
-/
-
-CREATE OR REPLACE TRIGGER TRI_membermanagement_mm_id BEFORE INSERT ON membermanagement
-FOR EACH ROW
-BEGIN
-   SELECT SEQ_membermanagement_mm_id.nextval
-   INTO :new.mm_id
-   FROM dual;
-END;
-
-/
-
-CREATE OR REPLACE TRIGGER TRI_member_mem_id BEFORE INSERT ON member
-FOR EACH ROW
-BEGIN
-   SELECT SEQ_member_mem_id.nextval
-   INTO :new.mem_id
-   FROM dual;
-END;
-
-/
-
-CREATE OR REPLACE TRIGGER TRI_mmtable_mm_id BEFORE INSERT ON mmtable
-FOR EACH ROW
-BEGIN
-   SELECT SEQ_mmtable_mm_id.nextval
-   INTO :new.mm_id
-   FROM dual;
-END;
-
-/
-
 CREATE OR REPLACE TRIGGER TRI_mm_table_mm_id BEFORE INSERT ON mm_table
 FOR EACH ROW
 BEGIN
    SELECT SEQ_mm_table_mm_id.nextval
    INTO :new.mm_id
-   FROM dual;
-END;
-
-/
-
-CREATE OR REPLACE TRIGGER TRI_movie_board_post_id BEFORE INSERT ON movie_board
-FOR EACH ROW
-BEGIN
-   SELECT SEQ_movie_board_post_id.nextval
-   INTO :new.post_id
-   FROM dual;
-END;
-
-/
-
-CREATE OR REPLACE TRIGGER TRI_notice_board_post_id BEFORE INSERT ON notice_board
-FOR EACH ROW
-BEGIN
-   SELECT SEQ_notice_board_post_id.nextval
-   INTO :new.post_id
-   FROM dual;
-END;
-
-/
-
-CREATE OR REPLACE TRIGGER TRI_posttable_post_id BEFORE INSERT ON posttable
-FOR EACH ROW
-BEGIN
-   SELECT SEQ_posttable_post_id.nextval
-   INTO :new.post_id
-   FROM dual;
-END;
-
-/
-
-CREATE OR REPLACE TRIGGER TRI_post_post_id BEFORE INSERT ON post
-FOR EACH ROW
-BEGIN
-   SELECT SEQ_post_post_id.nextval
-   INTO :new.post_id
    FROM dual;
 END;
 
@@ -400,17 +261,6 @@ BEGIN
 END;
 
 /
-
-CREATE OR REPLACE TRIGGER TRI_sports_board_post_id BEFORE INSERT ON sports_board
-FOR EACH ROW
-BEGIN
-   SELECT SEQ_sports_board_post_id.nextval
-   INTO :new.post_id
-   FROM dual;
-END;
-
-/
-
 
 
 
@@ -447,17 +297,23 @@ COMMENT ON COLUMN post_table.regdate IS '작성일';
 COMMENT ON COLUMN post_table.viewCnt IS '조회수';
 
 
-
-
 /* Create Views*/
 
 --총 게시글
 CREATE VIEW tot_post_view (post_id,regdate,empathize_cnt,view_cnt)
-AS SELECT P.post_id, P.regdate, E.empathCnt, P.viewCnt, P.category
+AS (SELECT P.post_id, P.regdate, E.empathCnt, P.viewCnt
 FROM post_table P LEFT OUTER JOIN (
                            SELECT post_id, COUNT(*) AS empathCnt
                            FROM empathize_table
                            GROUP BY post_id
                            ) E 
-ON P.post_id=E.post_id 
+ON P.post_id=E.post_id )
 ;
+
+
+SELECT * FROM comment_table;
+SELECT * FROM empathize_table;
+SELECT * FROM file_table;
+SELECT * FROM mm_table;
+SELECT * FROM post_table;
+SELECT * FROM tot_post_view;
