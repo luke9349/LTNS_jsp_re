66fe096c5610ea855eecb8f13fbcff3f4325187c
박성언 <tjddjs90@yahoo.com>
2020-10-30 23:33:19 +0900

    board Command 툴 작성

---
+++ src/main/java/com/model/board/BoardListDAO.java
@@ -0,0 +1,11 @@
+package main.java.com.model.board;
+
+public class BoardListDAO {
+	
+	public BoardListDTO[] getFreeList() {
+		BoardListDTO[] result = null;
+		
+		return result;
+	}
+
+}
---
+++ src/main/java/com/model/board/BoardListDTO.java
@@ -0,0 +1,5 @@
+package main.java.com.model.board;
+
+public class BoardListDTO {
+
+}
