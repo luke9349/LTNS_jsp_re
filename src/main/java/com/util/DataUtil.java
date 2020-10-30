4076d43dc3c5064a5fce7887708848798c249d16
박성언 <tjddjs90@yahoo.com>
2020-10-30 23:58:58 +0900

    datautil update resourceClose update

--- src/main/java/com/util/DataUtil.java
+++ src/main/java/com/util/DataUtil.java
@@ -10,18 +10,7 @@ import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
 public class DataUtil {
-	static Connection conn;
-	static PreparedStatement pstmt;
-	static Statement stmt;
-	static ResultSet rs;
 
-	
-	
-		
-	
-	
-	
-	
 	public static Connection getConnection() {
 
 		try {
@@ -36,9 +25,68 @@ public class DataUtil {
 		}
 		return null;
 	}
-	
 
-	
-	
+	public static void resourceClose(PreparedStatement pstmt, Connection conn) {
+		try {
+			if (pstmt != null && !pstmt.isClosed())
+				pstmt.close();
+			if (conn != null && !conn.isClosed())
+				conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void resourceClose(Statement stmt, Connection conn) {
+		try {
+			if (stmt != null && !stmt.isClosed())
+				stmt.close();
+			if (conn != null && !conn.isClosed())
+				conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void resourceClose(ResultSet rs, PreparedStatement pstmt, Connection conn) {
+		try {
+			if (rs != null && !rs.isClosed())
+				rs.close();
+			if (pstmt != null && !pstmt.isClosed())
+				pstmt.close();
+			if (conn != null && !conn.isClosed())
+				conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void resourceClose(ResultSet rs, Statement stmt, Connection conn) {
+		try {
+			if (rs != null && !rs.isClosed())
+				rs.close();
+			if (stmt != null && !stmt.isClosed())
+				stmt.close();
+			if (conn != null && !conn.isClosed())
+				conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void resourceClose(ResultSet rs, PreparedStatement pstmt, Statement stmt, Connection conn) {
+		try {
+			if (rs != null && !rs.isClosed())
+				rs.close();
+			if (pstmt != null && !pstmt.isClosed())
+				pstmt.close();
+			if (stmt != null && !stmt.isClosed())
+				stmt.close();
+			if (conn != null && !conn.isClosed())
+				conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 }
