212141301b3401234e4eed14dbbbfb37abf669c7
홍성혁 <markhong93@naver.com>
2020-11-02 20:42:39 +0900

    post model 작성

--- src/main/java/com/model/Post_Contents.java
+++ src/main/java/com/model/Post_Contents.java
@@ -1,19 +1,54 @@
 package main.java.com.model;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 
 public class Post_Contents {
 	private String title;
 	private String contentsText;
-	private Post_picture thumbnail;
+	private String thumbnailPath;
 	
+	private String contents;
 	Post_Contents(){
 	}
 	
 	Post_Contents(String filePath){
 		File file=new File(filePath);
-		
+		StringBuffer _contentsText = null;
+		try {
+			BufferedReader br=new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+			_contentsText=new StringBuffer();
+			
+			//제목
+			int title_index=0;
+			int thumbnail_index=0;
+			while(br.read()!=-1) {
+				String beforeStr=br.readLine();
+				String afterStr=null;
+				if(title_index==0) {
+					title=beforeStr.replaceAll("title","");
+					title_index++;
+					continue;
+				}//end title if
+				if(thumbnail_index==0) {
+					if(beforeStr.contains("<img")) {
+						thumbnailPath=beforeStr.substring(beforeStr.indexOf("src="));
+					}
+				}
+				afterStr=beforeStr.replaceAll("<br.*>", "\n").replaceAll("<.*>", "").replaceAll("&lt;", "<").replaceAll("&gt;", ">").replaceAll("&nbsp;"," ");
+				_contentsText.append(afterStr);
+			}//end while
+			br.close();
+		}catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public String getContentsText() {
@@ -28,20 +63,11 @@ public class Post_Contents {
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	public Post_picture getThumbnail() {
-		return thumbnail;
+	public String getThumbnailPath() {
+		return thumbnailPath;
 	}
-	//썸네일 변경
-	public void setThumbnail(Post_picture thumbnail) {
-		this.thumbnail = thumbnail;
-	}
-	
-
-	static class Maker{
-		static public Post_Contents makePost_Contents() {
-			Post_Contents post_contents= new Post_Contents();
-			return post_contents;
-		}
+	public void setThumbnailPath(String thumbnailPath) {
+		this.thumbnailPath = thumbnailPath;
 	}
 	
 	
