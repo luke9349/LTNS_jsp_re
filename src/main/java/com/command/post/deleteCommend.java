712baea9db2cbed4b4d664533b66538457951969
김현준 <kim@User-B>
2020-11-04 17:37:43 +0900

    delete 수정 ( 물리적 경로 파일까지 삭제 성공 )

--- src/main/java/com/command/post/deleteCommend.java
+++ src/main/java/com/command/post/deleteCommend.java
@@ -1,11 +1,14 @@
 package main.java.com.command.post;
 
+import java.io.File;
 import java.sql.SQLException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import main.java.com.command.Command;
+import main.java.com.model.post.FileWriteDAO;
+import main.java.com.model.post.FileWriteDTO;
 import main.java.com.model.post.WriteDAO;
 
 public class deleteCommend implements Command {
@@ -14,12 +17,13 @@ public class deleteCommend implements Command {
 	public void execute(HttpServletRequest request, HttpServletResponse response) {
 
 		int cnt = 0;
-		WriteDAO dao = new WriteDAO();
-		
-		int requestsdata = Integer.parseInt(request.getParameter("post_id"));
-		
+		FileWriteDAO dao = new FileWriteDAO();
+		FileWriteDTO[] arr = null;
+		int requestsdata = Integer.parseInt(request.getParameter("post_content"));
+		String realfilename;
 		try {
-			cnt = dao.post_delete(requestsdata);
+			arr = dao.view_cotent(requestsdata);
+			
 		} catch (SQLException e) {
 			e.printStackTrace();
 			System.out.println(requestsdata + " 캐치");
@@ -28,8 +32,28 @@ public class deleteCommend implements Command {
 		request.setAttribute("deletePost", cnt);
 		System.out.println("쿼리성공"+cnt);
 		
+		 realfilename = arr[0].real_filename; //패스 경로 꺼내옴 
+		System.out.println(realfilename);	
+		//경로에 있는 파일 삭제  
+		 if(realfilename != null) {
+				File fileDelete = new File(realfilename);
+				if( fileDelete.delete()) {
+					System.out.println(fileDelete + "파일삭제성공");
+				}else { System.out.println("실패");}
+				
+		 }// end if 
 		
-		
+		 
+		 //DB 삭제
+			try {
+				FileWriteDAO daos = new FileWriteDAO();
+				cnt = daos.file_delete(requestsdata);
+				request.setAttribute("delteOk", cnt);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		 	
 		
 	}
 
