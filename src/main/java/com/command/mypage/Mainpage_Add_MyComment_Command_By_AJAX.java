5da6e5a2266d53ca993f7b9771159c4f0a4103fe
홍성혁 <markhong93@naver.com>
2020-11-09 16:36:53 +0900

    ajax controller 갱신

---
+++ src/main/java/com/command/mypage/Mainpage_Add_MyComment_Command_By_AJAX.java
@@ -0,0 +1,16 @@
+package main.java.com.command.mypage;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import main.java.com.command.Command;
+
+public class Mainpage_Add_MyComment_Command_By_AJAX implements Command {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
